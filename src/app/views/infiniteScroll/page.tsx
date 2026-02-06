89f1973d7f1170632268e3d1a55a3036d90b4dbd
Dmitry <nsmg@yandex.ru>
2026-02-06 19:37:36 +0300

    Add infiniteScrollUsers page

---
+++ src/app/views/infiniteScroll/page.tsx
@@ -0,0 +1,20 @@
+import { dehydrate, HydrationBoundary, QueryClient, useQueryClient } from "@tanstack/react-query";
+import { fetchInfiniteScrollUsers } from "@/app/shared/api/api";
+
+export default async function infiniteScrollUsers() {
+  const queryClient = new QueryClient();
+
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["infiniteScrollUsers"],
+    queryFn: () => fetchInfiniteScrollUsers(1),
+    initialPageParam: 1,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      {/* <InfinityScrollUsers /> */}
+      No implemented.
+    </HydrationBoundary>
+  );
+}
